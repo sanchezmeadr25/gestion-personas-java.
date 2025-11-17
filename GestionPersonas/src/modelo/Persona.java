c25d1efa60913af192eb47a8d3922535de33d7b6
sanchezmeadr25-prog <sanchez.meadr25@triana.salesianos.edu>
2025-11-17 23:26:31 +0100

    GestionPersonas

---
+++ GestionPersonas/src/modelo/Persona.java
@@ -0,0 +1,17 @@
+package modelo;
+
+public class Persona {
+	 private String nombre;
+	    private int edad;
+
+	    public Persona(String nombre, int edad) {
+	        this.nombre = nombre;
+	        this.edad = edad;
+	    }
+
+	    public void mostrarDatos() {
+	        System.out.println("Nombre: " + nombre);
+	        System.out.println("Edad: " + edad);
+	    }
+	
+}
